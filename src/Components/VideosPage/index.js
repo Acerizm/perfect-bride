912cbbc195c31a685628d02664ebae64655828c0
EC2 Default User <ec2-user@ip-172-31-30-164.ap-southeast-1.compute.internal>
2019-10-16 08:23:05 +0000

    fixed video page

--- src/Components/VideosPage/index.js
+++ src/Components/VideosPage/index.js
@@ -1,25 +1,29 @@
-import React,{Component,useState} from "react";
+import React, { Component, useState } from "react";
 import "./index.css";
 
 //import window height here
-import {useWindowWidth, useWindowHeight} from "../ResponsiveComponent/index.js";
-import {minDesktopWidth,minMobileWidth,minTabletWidth} from "../ResponsiveComponent/index.js";
+import { useWindowWidth, useWindowHeight } from "../ResponsiveComponent/index.js";
+import { minDesktopWidth, minMobileWidth, minTabletWidth } from "../ResponsiveComponent/index.js";
+
+import "../../../node_modules/video-react/dist/video-react.css"; // import css
+
+import { Player } from 'video-react';
 
 const VideosPage = (props) => {
-    
+
     let currentWindowWidth = useWindowWidth();
     let currentVideosPage;
-    
-    if (currentWindowWidth >= minDesktopWidth){
+
+    if (currentWindowWidth >= minDesktopWidth) {
         currentVideosPage = <DesktopVideosPage/>;
     }
-    else if (currentWindowWidth >= minTabletWidth && currentWindowWidth < minDesktopWidth){
+    else if (currentWindowWidth >= minTabletWidth && currentWindowWidth < minDesktopWidth) {
         currentVideosPage = <TabletVideosPage/>;
     }
-    else{
+    else {
         currentVideosPage = <MobileVideosPage/>;
     }
-    
+
     return currentVideosPage;
 }
 
@@ -27,26 +31,55 @@ const DesktopVideosPage = (props) => {
     return (
         <div id="VideosPageContainer">
             <div id="PageTitle">Videos</div>
-            <div id="MainContainer">
-                <video id="MainVideo" width="320" height="240" autoplay>
-                    <source src="../../Videos/1.mov" type="video/mp4"/>
-                </video>
+            <div id="VideoContainer">
+                <Player
+                  playsInline
+                  poster="/assets/poster.png"
+                  src="https://res.cloudinary.com/inspirous/video/upload/v1571208534/Freelance/uxqijbbd5o5cncf9sd01.mp4"
+                  width = { "80%" }
+                  height = { "80%" }
+                  fluid={false}
+                  preload="none"
+                  videoId="Video1"
+                />
             </div>
         </div>
-        
+
     )
 }
 
 const TabletVideosPage = (props) => {
-    return(
-        <div>
+    return (
+        <div id="VideosPageContainer">
+                <div id="PageTitle">Videos</div>
+                <Player
+                  playsInline
+                  poster="/assets/poster.png"
+                  src="https://res.cloudinary.com/inspirous/video/upload/v1571208534/Freelance/uxqijbbd5o5cncf9sd01.mp4"
+                  width = { "80%" }
+                  height = { "50%" }
+                  fluid={false}
+                  preload="none"
+                  videoId="Video1"
+                />
         </div>
     )
 }
 
 const MobileVideosPage = (props) => {
     return (
-        <div>
+        <div id="VideosPageContainer">
+                <div id="PageTitle">Videos</div>
+                <Player
+                  playsInline
+                  poster="/assets/poster.png"
+                  src="https://res.cloudinary.com/inspirous/video/upload/v1571208534/Freelance/uxqijbbd5o5cncf9sd01.mp4"
+                  width = { "80%" }
+                  height = { "50%" }
+                  fluid={false}
+                  preload="none"
+                  videoId="Video1"
+                />
         </div>
     )
 }
